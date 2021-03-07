9fc801e980ea09640557cb1ad9c9330c02b28207
haywyre124 <80134404+haywyre124@users.noreply.github.com>
2021-03-06 20:19:53 -0600

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,21 @@
+var $container = document.getElementById('container');
 
+function Game() {
+  this.viewport = document.createElement('canvas');
+  this.context = viewport.getContext('2d');
+  
+  this.viewport.width = 800;
+  this.viewport.height = 600;
+  
+  $container.insertBefore(this.viewport, $container.firstChild);
+  //random txt
+  this.context.font = '32px arial';
+  this.context.fillText('I\'m Feeling lucky', 5, 50, 800);
+  
+  return this;
+}
+//instantiate the game in global
+windwo.game = new Game()
+
+//export the game as a module
+module.exports = game();
